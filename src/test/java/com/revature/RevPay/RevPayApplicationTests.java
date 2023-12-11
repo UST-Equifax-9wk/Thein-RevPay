0e886ec4e7a6be5668314073644641d9b8cae25b
thein-than-hein <theinthan912@revature.net>
2023-12-11 15:47:14 -0800

    Initial commit

---
+++ src/test/java/com/revature/RevPay/RevPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
