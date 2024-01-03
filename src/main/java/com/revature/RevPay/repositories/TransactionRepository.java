21912a4b9a4895969157ec7517679662cc202586
thein-than-hein <theinthan912@revature.net>
2024-01-03 06:53:42 -0800

    Remove old codes

--- src/main/java/com/revature/RevPay/repositories/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.RevPay.repositories;
-
-import com.revature.RevPay.entities.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-}
